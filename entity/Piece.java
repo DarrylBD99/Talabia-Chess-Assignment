d47109f44ef5d363398cf7edaf7c70e8d6972bbb
DarrylBD99 <darrylhoBD@gmail.com>
2024-01-10 08:04:45 +0800

    Update Piece.java

--- entity/Piece.java
+++ entity/Piece.java
@@ -1,12 +1,12 @@
 public abstract class Piece {
     protected int playerIndex;
 
-    boolean checkValidMove(int startX, int startY, int endX, int endY)
+    boolean checkValidMove(int start_x, int start_y, int end_x, int end_y)
     {
         // Check if the move is within the board boundaries
-        if (endX < 0 || endX >= ChessView.ROWS || endY < 0 || endY >= ChessView.COLS) return false;
+        if (end_x < 0 || end_x >= ChessView.ROWS || end_y < 0 || end_y >= ChessView.COLS) return false;
         // Check if the move is either horizontal or vertical
-        if (startX != endX && startY != endY) return false;
+        if (start_x != end_x && start_y != end_y) return false;
         
         return true;
     }
