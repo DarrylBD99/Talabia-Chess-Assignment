a8721607a66ae18158f9565ab18eeb2ffe7efc6a
DarrylBD99 <darrylhoBD@gmail.com>
2024-01-14 21:47:26 +0800

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,6 +1,13 @@
 public abstract class Piece {
     protected int playerIndex;
     protected int PieceType;
+    /*
+     * 0: Point
+     * 1: HourGlass
+     * 2: Time
+     * 3: Plus
+     * 4: Sun
+     */
 
     int getPlayerIndex() { return playerIndex; }
 
@@ -9,4 +16,10 @@ public abstract class Piece {
     int getPieceType() { return PieceType; }
 
     void setPieceType(int PieceType) { this.PieceType = PieceType; }
+
+    public Piece(int playerIndex, int PieceType)
+    {
+        this.playerIndex = playerIndex;
+        this.PieceType = PieceType;
+    }
 }
