8e8cd539ac55fc06755c3ed2c3d093d92c28b065
DarrylBD99 <darrylhoBD@gmail.com>
2024-01-23 18:02:11 +0800

    Update PieceView.java

--- view/PieceView.java
+++ view/PieceView.java
@@ -1,15 +1,12 @@
 import javax.swing.ImageIcon;
+import java.awt.image.BufferedImage;
 import java.net.URL;
 
 public class PieceView {
+    BufferedImage original_image;
     ImageIcon icon;
     URL image_url;
-
-    // Retrieves the visual icon of the piece.
-    ImageIcon getIcon() {
-        return icon;
-    }
-
+    
     public PieceView(Piece model)
     {
         int playerIndex = model.getPlayerIndex();
@@ -17,6 +14,24 @@ public class PieceView {
 
         // Define the base path as a constant or configuration option.
         String path = String.format("resources/%d/%s.png", playerIndex, pieceType.toString().toLowerCase());
+        image_url = getClass().getResource(path);
+        icon = new ImageIcon(image_url);
+        
+        // Print information about the loaded image.
+        System.out.println("Image path: " + image_url); 
+        System.out.println("Icon width: " + icon.getIconWidth());
+        System.out.println("Icon height: " + icon.getIconHeight());
+    }
+
+    public PieceView(Piece model, int type)
+    {
+        int playerIndex = model.getPlayerIndex();
+        PieceType pieceType = model.getPieceType();
+
+        // Define the base path as a constant or configuration option.
+        String path = String.format("resources/%d/%s.png", playerIndex, pieceType.toString().toLowerCase());
+        if (type > 0) path = String.format("resources/%d/%s_%d.png", playerIndex, pieceType.toString().toLowerCase(), type);
+
         image_url = getClass().getResource(path);
         icon = new ImageIcon(image_url);
         
